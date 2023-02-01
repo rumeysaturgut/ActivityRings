ca782274a238f07d00b6e388cb32c01a847a2c2a
Rumeysa Turgut <rumeysa.turgut@kitapyurdu.com>
2023-02-01 16:34:33 +0300

    Initial Commit

---
+++ ActivityRings/ActivityRingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActivityRingsApp.swift
+//  ActivityRings
+//
+//  Created by Rumeysa Turgut on 1.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct ActivityRingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
